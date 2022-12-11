4f23f0cbb7af0f7bca640845c0b33909b7fc5f20
lllosya <119103857+lllosya@users.noreply.github.com>
2022-12-11 22:39:26 +0300

    Update task 3.py

--- Лабораторная 5/task 3.py
+++ Лабораторная 5/task 3.py
@@ -2,12 +2,14 @@ import random
 
 
 def get_unique_list_numbers(x, y, n) -> list[int]:
-    while True:
-        unique_num = [random.randint(x, y) for _ in range (n)]
-        if len(unique_num) == len(set(unique_num)):
-            return unique_num
-        else:
+    list_ = []
+    while len(list_) < n:
+        unique_num = random.randint(x, y)
+        if unique_num in list_:
             continue
+        else:
+            list_.append(unique_num)
+    return list_
 
 
 list_unique_numbers = get_unique_list_numbers(-10, 10, 15)
